d38ce7859b0b0d9a46e3b4aa5e3b5e2023a2aa42
colinbgood <colinbgood@yahoo.com>
2021-08-06 18:57:55 -0700

    Add files via upload

---
+++ Gem/Code/atomsampleviewergem_tools_files.cmake
@@ -0,0 +1,16 @@
+#
+# Copyright (c) Contributors to the Open 3D Engine Project.
+# For complete copyright and license terms please see the LICENSE at the root of this distribution.
+#
+# SPDX-License-Identifier: Apache-2.0 OR MIT
+#
+#
+
+set(FILES
+    Tools/AtomSampleViewerToolsSystemComponent.h
+    Tools/AtomSampleViewerToolsSystemComponent.cpp
+    Tools/MaterialFunctors/StacksShaderCollectionFunctorSourceData.h
+    Tools/MaterialFunctors/StacksShaderCollectionFunctorSourceData.cpp
+    Tools/MaterialFunctors/StacksShaderInputFunctorSourceData.h
+    Tools/MaterialFunctors/StacksShaderInputFunctorSourceData.cpp
+)
